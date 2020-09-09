030bf83ac22c9f2528fcbb9e45a6d4afd5c39fc7
Navithu sriyananda <43490534+NavithuSriyananda@users.noreply.github.com>
2020-09-09 15:07:39 +0530

    Add files via upload

---
+++ Slave/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run command to allocate the default or custom system resources (memory) to JMeter at 'docker run'
+sed -i 's/\("${HEAP:="\)\(.*\)\("}"\)/\1-Xms'512m' -Xmx'1024m' -XX:MaxMetaspaceSize='2048m'\3/' ${JMETER_BIN}/jmeter &&
+$JMETER_HOME/bin/jmeter-server \
+    -Dserver.rmi.localport=50000 \
+    -Dserver_port=1099 \
+    -Jserver.rmi.ssl.disable=true \
+    -Djava.rmi.server.hostname=$PublicIP
+
+exec "$@"
